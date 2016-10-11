2c6357fa58468bc7927687fabcf31ce806537a98
Chenje Katanda <chenje@ssn.co.zw>
2016-10-11 14:41:30 -0400

    Change price of banana and add vacuum to cart

--- shop/shoppingcart.rb
+++ shop/shoppingcart.rb
@@ -59,8 +59,8 @@ end
 
 
 joshs_cart = ShoppingCart.new
-banana = Fruit.new("Banana", 20)
-#vaccuum = Houseware.new("Vaccuum", 150)
+banana = Fruit.new("Banana", 10)
+vaccuum = Houseware.new("Vaccuum", 150)
 oj = Item.new("Orange Juice", 10)
 rice = Item.new("Rice", 1)
 anchovies = Item.new("Anchovies", 2)
@@ -68,7 +68,7 @@ anchovies = Item.new("Anchovies", 2)
 joshs_cart.add_item(oj)
 joshs_cart.add_item(banana)
 joshs_cart.add_item(rice)
-#joshs_cart.add_item(vaccuum)
+joshs_cart.add_item(vaccuum)
 joshs_cart.add_item(anchovies)
 joshs_cart.add_item(anchovies)
 puts joshs_cart.checkout
